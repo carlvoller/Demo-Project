e5a825153af25153afd6d9c817c7d716e739bf3a
YJ Soon <yjsoon@tinkertanker.com>
2022-06-06 16:03:47 +0800

    Added a Stack to Events

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function EventsScreen() {
+function EventsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Settings!</Text>
     </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function EventsStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="EventsHome" component={EventsScreen} />
+    </Stack.Navigator>
   );
 }
