39495358540b95e80bbf4a38aacec10fe848a8f6
YJ Soon <yjsoon@tinkertanker.com>
2022-06-07 10:06:13 +0800

    Added a second screen to the Events Stack

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,11 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/native";
 
 function EventsScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
+      <Text>Events first screen!</Text>
+      <Button
+        title="Go to second screen"
+        onPress={() => {
+          navigation.navigate("EventsSecondScreen");
+        }}
+      />
+    </View>
+  );
+}
+
+function EventsSecondScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "hotpink",
+      }}
+    >
+      <Text>Events second screen!</Text>
     </View>
   );
 }
@@ -16,6 +40,7 @@ export default function EventsStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="EventsHome" component={EventsScreen} />
+      <Stack.Screen name="EventsSecondScreen" component={EventsSecondScreen} />
     </Stack.Navigator>
   );
 }
